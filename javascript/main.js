d06588047b50eae4e5cdb6d944323dede2ad37be
nirglus <nirgluskin@gmail.com>
2023-09-29 21:03:55 +0300

    add select and buttons

--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,14 @@
 function popularWeekFetch(week){
 
-}
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: ``
-    }
-  };
+}  
+
+function popularDayFetch(day){
   
-  fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+}
+function fetchPopular(page=1){
+  fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}&api_key=38aa22b0b8ec6f9efab4ea43ec7c4adc`)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
-
-function popularDayFetch(day){
-
+  
 }
