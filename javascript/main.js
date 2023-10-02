835e7058cd52387d4712d3d9e157cd25406e7af2
nirglus <nirgluskin@gmail.com>
2023-10-02 19:47:07 +0300

    remove undefined funcs

--- javascript/main.js
+++ javascript/main.js
@@ -1,10 +1,4 @@
-function popularWeekFetch(week){
-
-}  
-
-function popularDayFetch(day){
-  
-}
+// Popular movies fetch
 function fetchPopular(page=1){
   fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=${page}&api_key=38aa22b0b8ec6f9efab4ea43ec7c4adc`)
     .then(response => response.json())
