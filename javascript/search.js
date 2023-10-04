14d1c65850e6a0139775621cd07633a3285efcac
nirglus <nirgluskin@gmail.com>
2023-10-04 07:52:30 +0300

    add searchByName func

--- javascript/search.js
+++ javascript/search.js
@@ -1,5 +1,6 @@
 // Search by name fetch
 function searchByName(){
+    const movName = document.getElementById("searchNameInput").value;
     const options = {
         method: 'GET',
         headers: {
@@ -8,10 +9,19 @@ function searchByName(){
         }
       };
       
-      fetch('https://api.themoviedb.org/3/search/movie?include_adult=false&language=en-US&page=1', options)
+      fetch(`https://api.themoviedb.org/3/search/movie?query=${movName}&include_adult=false&language=en-US&page=1`, options)
         .then(response => response.json())
         .then(data => {
-            
+            const movieNames = document.getElementById("movieNames");
+            const moviesArr = data.results;
+            moviesArr.forEach((element, index) => {
+              movieNames.innerHTML += `<div id="mov-${index}">
+              <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}">
+              <h1>${element.original_title}</h1>
+              <p>Released on ${element.release_date}</p>
+              <p>${element.vote_average}</p>`
+            });
         } )
         .catch(err => console.error(err));
 }
+document.getElementById("searchNameBtn").addEventListener("click", searchByName);
