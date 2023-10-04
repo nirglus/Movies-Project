94b7be8f364078772d3c814d67a44d671c57cc92
nirglus <nirgluskin@gmail.com>
2023-10-04 14:31:18 +0300

    change cards style

--- javascript/main.js
+++ javascript/main.js
@@ -7,12 +7,18 @@ function fetchPopular(page=1){
       const topMovies = document.getElementById("popMovCard");
       const moviesArr = data.results;
       moviesArr.forEach((element, index) => {
-        topMovies.innerHTML += `<div id="mov-${index}">
+        topMovies.innerHTML += `
+        <div class="col-md-3 mb-4">
+        <div class="movieCard" id="mov-${index}">
         <img src="https://image.tmdb.org/t/p/w500/${element.poster_path}">
-        <h1>${element.original_title}</h1>
+        <div class="card-body">
+        <h2>${element.original_title}</h2>
         <p>Released on ${element.release_date}</p>
         <p>${element.vote_average}</p>
-        <button id="like-${index}-${page}">Add to favorite</button>`
+        <button id="like-${index}-${page}">Add to favorite</button>
+        </div>
+        </div>
+        </div>`
       });
     })
     .catch(err => console.error(err));
