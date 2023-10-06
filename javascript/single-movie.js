cb581691be46f2c14164ed21497ca39f3359af90
nirglus <nirgluskin@gmail.com>
2023-10-06 15:37:08 +0300

    set style to strong

--- javascript/single-movie.js
+++ javascript/single-movie.js
@@ -23,10 +23,10 @@ function singleMovFetch(){
           <div class="col-md-8">
             <div class="card-body">
               <h2 class="card-title">${data.original_title}</h2>
-              <p class="card-text"><strong>Genre:</strong> ${data.genres[0].name}, ${data.genres[1].name}</p>
-              <p id="mov-about" class="card-text"><strong>Description:</strong> ${data.overview}</p>
-              <p class="card-text"><strong>Actors:</strong> <span id="actorsSpan"></span></p>
-              <p class="card-text"><small class="text-muted"><strong>Released on:</strong> ${data.release_date}</small></p>
+              <p class="card-text"><strong>Genre: </strong>${data.genres[0].name}, ${data.genres[1].name}</p>
+              <p id="mov-about"><strong>Description: </strong>${data.overview}</p>
+              <p class="card-text"><strong>Actors: </strong><span id="actorsSpan"></span></p>
+              <p class="card-text"><small class="text-muted"><b>Released on: </b>${data.release_date}</small></p>
               <div class="text-right">
                 <button class="fav-btn btn btn-outline-danger" id="like-${movID}"><i class="bi bi-heart"></i> Add to favorites</button>
               </div>
