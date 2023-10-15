7460f151f8638e3a888d6a91ed1f8f182450c7f7
nirglus <nirgluskin@gmail.com>
2023-10-15 07:05:33 +0300

    add js for favs

--- javascript/favorites.js
+++ javascript/favorites.js
@@ -1,3 +1,8 @@
-const FAV_ARR = [];
-
-export {FAV_ARR};
+const favStorage = JSON.parse(localStorage.getItem("favorites"));
+console.log(favStorage);
+const favCard = document.getElementById("favMovCard");
+favStorage.forEach((element) => {
+    if(element != null){
+        favCard.innerHTML += element;
+    }
+})
